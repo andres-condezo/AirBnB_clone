385194bddb61c3fbbc61fd0478050e4b5f012d37
Manuel-condori <2828@holbertonschool.com>
2021-06-27 11:30:59 -0500

    first commit base  model

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Base model class"""
+import uuid
+from datetime import datetime
+
+
+
+
+class BaseModel:
+    """Defines all common attributes-methods for other classes"""
+    def __init__(self, *args, **kwargs):
+        """Initializes object"""
+        if kwargs:
+            for key, val in kwargs.items():
+                if key != "__class__":
+                    if key == "create_at":
+                        val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    if key == "updated_at":
+                        val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, val)
+        else:            
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+    
+
+    def __str__(self):
+        """Returns string representation"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+    
+    def save(self):
+        """Updates the public instance attr with current datetime"""
+        self.updated_at = datetime.now()
+    
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__"""
+        dict_returned = dict(self.__dict__)
+        dict_returned["__class__"] = self.__class__.__name__
+        dict_returned["created_at"] = self.created_at.isoformat()
+        dict_returned["updated_at"] = self.updated_at.isoformat()
+        return dict_returned    
