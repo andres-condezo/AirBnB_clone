80ea34751235179074cca879d90fc5c042d6a62a
adrs condezo <ra.condezomonge@gmail.com>
2021-06-29 11:17:32 -0500

    Fix typo in init method in the created if

--- models/base_model.py
+++ models/base_model.py
@@ -7,17 +7,19 @@ import models
 
 class BaseModel:
     """Defines all common attributes-methods for other classes"""
+
     def __init__(self, *args, **kwargs):
         """Initializes object"""
         if kwargs:
             for key, val in kwargs.items():
                 if key != "__class__":
-                    if key == "create_at":
+                    if key == "created_at":
                         val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
                     if key == "updated_at":
                         val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, val)
-        else:            
+
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -26,7 +28,7 @@ class BaseModel:
     def __str__(self):
         """Returns string representation"""
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
-    
+
     def save(self):
         """Updates the public instance attr with current datetime"""
         self.updated_at = datetime.now()
@@ -38,4 +40,4 @@ class BaseModel:
         dict_returned["__class__"] = self.__class__.__name__
         dict_returned["created_at"] = self.created_at.isoformat()
         dict_returned["updated_at"] = self.updated_at.isoformat()
-        return dict_returned    
+        return dict_returned
