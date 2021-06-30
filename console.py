6a0a3a6be9223dd8286e75d2b5288a5edb4f730f
adrs condezo <ra.condezomonge@gmail.com>
2021-06-30 01:18:45 -0500

    add create, show, destroy and all functions

--- console.py
+++ console.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """This module contains the entry point of the command interpreter"""
 import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """The command interpreter class"""
     prompt = ('(hbnb) ')
+    my_classes = ["BaseModel"]
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -19,6 +23,74 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Doesn't execute anything"""
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of  a class model"""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in self.my_classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance"""
+        tokens = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif tokens[0] not in self.my_classes:
+            print("** class doesn't exist **")
+        elif len(tokens) < 2:
+            print("** instance id missing **")
+        else:
+            key_for_search = "{}.{}".format(tokens[0], tokens[1])
+            all_objects = storage.all()
+
+            if key_for_search in all_objects.keys():
+                print(all_objects[key_for_search])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        tokens = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif tokens[0] not in self.my_classes:
+            print("** class doesn't exist **")
+        elif len(tokens) < 2:
+            print("** instance id missing **")
+        else:
+            key_for_search = "{}.{}".format(tokens[0], tokens[1])
+            all_objects = storage.all()
+
+            if key_for_search in all_objects.keys():
+                del all_objects[key_for_search]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances
+            based or not on the class name"""
+        tokens = arg.split()
+        all_list = []
+        all_objects = storage.all()
+
+        if len(tokens) < 1:
+            for value in all_objects.values():
+                all_list.append(str(value))
+            print(all_list)
+        elif tokens[0] in self.my_classes:
+            class_for_search = tokens[0]
+            for key, value in all_objects.items():
+                if class_for_search in key:
+                    all_list.append(str(value))
+            print(all_list)
+        else:
+            print("** class doesn't exist **")
 
 
 if __name__ == '__main__':
