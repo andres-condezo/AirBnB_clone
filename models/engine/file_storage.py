dc503ff145b71399c563d465b10e27c21823cc02
Manuel-condori <2828@holbertonschool.com>
2021-06-30 18:56:21 -0500

    add imports

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,9 @@ from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
 from models.review import Review
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
 
 
 class FileStorage:
